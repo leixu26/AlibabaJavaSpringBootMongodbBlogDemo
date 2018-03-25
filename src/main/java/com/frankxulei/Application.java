9e8e69f0132b3b936c14452c129990e8c7334425
frankxulei <403341202@qq.com>
2018-03-25 18:24:29 +0800

    Add repository and View pages

--- src/main/java/com/frankxulei/Application.java
+++ src/main/java/com/frankxulei/Application.java
@@ -1,19 +1,27 @@
 package com.frankxulei;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.frankxulei.models.User;
+import com.frankxulei.repository.UsersRepository;
 /**
  * @Package: com.frankxulei 
  * @ClassName: Application  
  * @Description: Java Spring Data实战阿里云Mongodb 
  * @author: Frank Xu Lei 
- * @Date : 2018-5-26 
+ * @Date : 2018-5-4
 */
 @SpringBootApplication
 public class Application implements CommandLineRunner {
 
+	/**
+	 * Spring 自动装配 
+	 */
+	@Autowired
+	private UsersRepository repository;
 	/**
 	 * 主函数 执行入口
 	 */
@@ -23,9 +31,13 @@ public class Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-		
+		//repository.deleteAll();
+		// fetch all users
+		System.out.println("查找所有用户");
+		System.out.println("-------------------------------");
+		for (User user2 : repository.findAll()) {
+			System.out.println(user2.getName());
+		}
+		 
 	}
-
-	 
 }
